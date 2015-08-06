666211d80e0f318a27d4eb594c9a30eb1b606f7c
Kehinde Shittu <she2_adex@yahoo.com>
2015-08-06 15:03:19 +0100

    added view controllers

---
+++ Executer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Executer
+//
+//  Created by Kehinde Shittu on 8/6/15.
+//  Copyright (c) 2015 Kehesjay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
