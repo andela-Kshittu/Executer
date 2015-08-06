a698e4624a4e36b36c8ea70a0718adf61346fdc2
Ahmed Onawale <ahmed.onawale@andela.com>
2015-08-06 16:10:38 +0100

    design events view

---
+++ Executer/SyncCalendarViewController.h
@@ -0,0 +1,13 @@
+//
+//  SyncCalendarViewController.h
+//  Executer
+//
+//  Created by Ahmed Onawale on 8/6/15.
+//  Copyright (c) 2015 Kehesjay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SyncCalendarViewController : UIViewController
+
+@end
